1fb7ccf2f37b946d1d0bffd56d2bb5555e4b977f
Nikola <nikolavukic333@gmail.com>
2024-01-07 22:28:30 +0100

    added borrow count update on borrow

--- library-app/local/service/borrow_service.go
+++ library-app/local/service/borrow_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -24,6 +25,11 @@ type Member struct {
 	BorrowCnt int
 }
 
+type UpdateDTO struct {
+	SSN      string
+	NewCount int
+}
+
 type BorrowService struct {
 	borrowRepo *repository.BorrowRepository
 }
@@ -41,20 +47,28 @@ func (s *BorrowService) CreateNewBorrow(ctx context.Context, borrowDTO dto.Borro
 
 	client := &http.Client{}
 
-	Member := getMemberBySSN(borrowDTO.SSN, client)
-	if Member.BorrowCnt > 2 {
+	member, err := getMemberBySSN(borrowDTO.SSN, client)
+	if err != nil {
+		return err
+	}
+	if member.BorrowCnt > 2 {
 		return errors.New("Member is already borrowing three books.")
 	}
 
+	err = increaseBorrowCount(member, client)
+	if err != nil {
+		return err
+	}
+
 	newBorrow := model.Borrow{
-		UserId: Member.Id,
+		UserId: member.Id,
 		Title:  borrowDTO.Title,
 		Author: borrowDTO.Author,
 		ISBN:   borrowDTO.ISBN,
 		From:   time.Now(),
 	}
 
-	err := s.borrowRepo.SaveBorrow(ctx, newBorrow)
+	err = s.borrowRepo.SaveBorrow(ctx, newBorrow)
 	if err != nil {
 		log.Printf("Error registering borrow: %v\n", err)
 		return err
@@ -64,30 +78,62 @@ func (s *BorrowService) CreateNewBorrow(ctx context.Context, borrowDTO dto.Borro
 }
 
 func isInvalidDTO(borrowDTO dto.BorrowDTO) bool {
-	titleMissing := len(borrowDTO.Title) == 0
 	ssnMissing := len(borrowDTO.SSN) == 0
-	return titleMissing || ssnMissing
+	titleMissing := len(borrowDTO.Title) == 0
+	authorMissing := len(borrowDTO.Author) == 0
+	isbnMissing := len(borrowDTO.ISBN) == 0
+	return titleMissing || ssnMissing || authorMissing || isbnMissing
 }
 
-func getMemberBySSN(ssn string, client *http.Client) Member {
+func getMemberBySSN(ssn string, client *http.Client) (Member, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:8080/get?ssn=%s", ssn), nil)
+	if err != nil {
+		return Member{}, fmt.Errorf("Error creating HTTP request: %v", err)
+	}
+
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return Member{}
+		return Member{}, fmt.Errorf("Error sending HTTP request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Unexpected status code:", resp.StatusCode)
-		return Member{}
+		return Member{}, fmt.Errorf("Unexpected status code: %v", resp.StatusCode)
 	}
 
 	var m Member
 	if err := json.NewDecoder(resp.Body).Decode(&m); err != nil {
-		fmt.Println("Error decoding response body:", err)
-		return Member{}
+		return Member{}, fmt.Errorf("Error decoding response body: %v", err)
 	}
 
-	return m
+	return m, nil
+}
+
+func increaseBorrowCount(member Member, client *http.Client) error {
+	var updateDTO UpdateDTO
+	updateDTO.SSN = member.SSN
+	updateDTO.NewCount = member.BorrowCnt + 1
+	requestBody, err := json.Marshal(updateDTO)
+	if err != nil {
+		return fmt.Errorf("Error encoding UpdateDTO into JSON: %v", err)
+	}
+
+	req, err := http.NewRequest("PUT", "http://localhost:8080/update-borrow-count", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return fmt.Errorf("Error creating HTTP request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("Error sending HTTP request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Unexpected status code: %v", resp.StatusCode)
+	}
+
+	return nil
 }
