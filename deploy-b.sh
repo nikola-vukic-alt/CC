879edabebaaacc5763ad27d44bcc557b40032fcb
nikola-vukic-alt <vukic.e268.2023@uns.ac.rs>
2024-01-09 16:10:55 +0100

    kubernetes and scripts to deploy/stop app [stop ci]

---
+++ deploy-b.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Apply Volumes and Claims
+kubectl apply -f k8s/volumes/central-host-pv.yaml
+kubectl apply -f k8s/volumes/ns-host-pv.yaml
+kubectl apply -f k8s/volumes/bg-host-pv.yaml
+kubectl apply -f k8s/volumes/nis-host-pv.yaml
+echo
+
+# Apply Deployments
+kubectl apply -f k8s/deployments/central-deployment.yaml
+kubectl apply -f k8s/deployments/ns-deployment.yaml 
+kubectl apply -f k8s/deployments/bg-deployment.yaml 
+kubectl apply -f k8s/deployments/nis-deployment.yaml 
+echo
+
+# Apply Services
+kubectl apply -f k8s/services/central-service.yaml
+kubectl apply -f k8s/services/ns-service.yaml
+kubectl apply -f k8s/services/bg-service.yaml
+kubectl apply -f k8s/services/nis-service.yaml
+echo 
+
+# Apply Ingress
+kubectl apply -f k8s/ingress/library-ingress.yaml
+echo 
+
+echo "All resources applied."
